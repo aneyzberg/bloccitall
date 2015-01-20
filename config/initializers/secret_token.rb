fa7473b778739b34747ed0f30ca63fbed880dc96
Anna Neyzberg <aneyzberg@gmail.com>
2015-01-20 19:28:34 +0000

    First commit for bloccitall

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccitall::Application.config.secret_key_base = 'ba71b889aa238bd0836311054ae2b12a442e7d30005082a1b7cbfc787ad97d3d083818e9afd087836d21282454a085384c7a46a2b879c796579c9136240e4d27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bloccitall::Application.config.session_store :cookie_store, key: '_bloccitall_session'
